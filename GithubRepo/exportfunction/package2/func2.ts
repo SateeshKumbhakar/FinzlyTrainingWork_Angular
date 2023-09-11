67cafdfa396ff0d3c1852d38218514ef5eb144b7
SATEESH KUMBHAKAR <91566141+SateeshKumbhakar@users.noreply.github.com>
2023-09-12 02:10:33 +0530

    Delete GithubRepo/exportfunction directory

--- GithubRepo/exportfunction/package2/func2.ts
+++
@@ -1,4 +0,0 @@
-import { sum } from "../packageOne/func";
-
-// function from different package by import export
-sum(10,20);
--- GithubRepo/exportfunction/packageOne/func.ts
+++
@@ -1,7 +0,0 @@
-export function sum (a:number,b:number){
-    let ans = a+b;
-    console.log(`
-    This is sum method  and is ${ans}
-  `);
-
-}
