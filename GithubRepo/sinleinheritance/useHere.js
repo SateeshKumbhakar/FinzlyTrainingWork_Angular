1aac3a8cba04eee10ddafd6e16a82d90714f1de4
SATEESH KUMBHAKAR <91566141+SateeshKumbhakar@users.noreply.github.com>
2023-09-12 02:11:08 +0530

    Delete GithubRepo/sinleinheritance directory

--- GithubRepo/sinleinheritance/useHere.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var classemanager_1 = require("./classemanager");
-var manager = new classemanager_1.Manager(1001, "Sateesh", 123, "sales");
-manager.displayManagerData();
-console.log(manager.id);
-console.log(manager.name);
-console.log(manager.mid);
-console.log(manager.category);
